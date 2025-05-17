af9bfea4c22699b27da5440ffb2ed3414c888ea5
TheRohit <rohitmeshram000@gmail.com>
2025-05-18 01:32:23 +0530

    add analytics and react compiler

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async rewrites() {
+    return [
+      {
+        source: "/wassup/:match*",
+        destination: "https://portfolio.rohitm.dev/_vercel/insights/:match*",
+      },
+    ];
+  },
+  experimental: {
+    reactCompiler: true,
+  },
 };
 
 export default nextConfig;
